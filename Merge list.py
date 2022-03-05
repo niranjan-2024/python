cfa822e397d917bed6691eb9431d31678418d620
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2022-03-06 00:30:16 +0530

    Create Merge list.py

---
+++ Merge list.py
@@ -0,0 +1,12 @@
+a = ["i"]
+b = ["am"]
+c = ["niranjan"]
+s = a+b+c 
+print(s)
+d = ["i","learn","python"]
+s.extend(d)
+print(s)
+e = ["i","am","mad"]
+for element in e:
+   s.append(element)
+print(s)
